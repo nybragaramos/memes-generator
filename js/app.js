c5ec6889e9ff6b8190259f218a719da834808b86
nadjayanna <nadjayannabr@gmail.com>
2018-08-16 13:58:58 +0200

    add button do submit form

--- js/app.js
+++ js/app.js
@@ -1,34 +1,40 @@
+// Create an image object. This is not attached to the DOM and is not part of the page.
+const canvas=document.getElementById("memeImage");
+const ctx=canvas.getContext("2d");
+
 //function to handle the file selection
-function handleFileSelect(evt) {
-  const files = evt.target.files; // FileList object
-  console.log(files);
+function loadImage() {
+  const input = $('#files')[0]; // FileList object
+  console.log(input);
+  const file = input.files[0];
   // Only process image files.
-  if (files[0].type.match('image.*')) {
+  if (input.files[0].type.match('image.*')) {
     const reader = new FileReader();
     // Closure to capture the file information.
     reader.onload = (function(file) {
       return function(e) {
-        loadAndDrawImage(reader.result);
+        drawImage(reader.result);
       };
-    })(files[0]);
+    })(input.files[0]);
     // Read in the image file as a data URL.
-    reader.readAsDataURL(files[0]);
+    reader.readAsDataURL(input.files[0]);
   }
 }
 
-function loadAndDrawImage(src)
+function drawImage(src)
 {
-  // Create an image object. This is not attached to the DOM and is not part of the page.
-  const c=document.getElementById("memeImage");
-  const ctx=c.getContext("2d");
   const image = new Image();
   // When the image has loaded, draw it to the canvas
   image.onload = function()
   {
       // draw image...
-      ctx.drawImage(this,0,0, c.width, c.height);
+      ctx.drawImage(this,0,0);
   }
   // Now set the source of the image that we want to load
   image.src = src;
 }
-$('#files').on('change', handleFileSelect);
+
+$("#create").submit(function(e) {
+    e.preventDefault();
+    loadImage();
+});
